2d677b24c67844b650005a5433a1672dacd733b7
a.nizelskiy <a.nizelskiy@axelot.ru>
2025-01-17 00:03:05 +0300

    инициализация frontend проекта

---
+++ image-filter-app/src/app/components/filter-selector/filter-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-selector.component.html',
+  styleUrl: './filter-selector.component.scss'
+})
+export class FilterSelectorComponent {
+
+}
